f05f453c1715c892f753bae62dbd65908ec36708
Javad Mustafayev <muhendees@gmail.com>
2024-01-13 15:47:26 -0800

    Add job launcher function

--- go.mod
+++ go.mod
@@ -5,23 +5,36 @@ go 1.19
 require (
 	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.15.0
+	k8s.io/api v0.29.0
+	k8s.io/apimachinery v0.29.0
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.110.1 // indirect
+	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 )
--- handler.go
+++ handler.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"context"
+	"strings"
+
 	"database/sql"
 	"fmt"
+
+	"github.com/spf13/viper"
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/spf13/viper"
 )
@@ -38,7 +48,7 @@ func getJob() {
 		jobName := fmt.Sprintf("print-operator-%v", id)
 		entryCommand := "/bin/bash /opt/start.sh"
 
-		launchK8sJobOperator(&jobName, &containerImage, &entryCommand, id, name, url)
+		launchK8sJob(&jobName, &containerImage, &entryCommand, id, name, url)
 	}
 	defer db.Close()
 	defer rows.Close()
@@ -46,4 +56,106 @@ func getJob() {
 		log.Error().Msg("Error during iteration.")
 		log.Err(err)
 	}
+}
+
+func launchK8sJob(jobName *string, image *string, cmd *string, id int) {
+	log := loggerGet()
+	clientset := connectToKubernetes()
+	jobs := clientset.BatchV1().Jobs(viper.GetString("NAMESPACE"))
+	var backOffLimit int32 = 0
+	var ttlSecondsAfterFinished int32 = 30
+
+	jobSpec := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      *jobName,
+			Namespace: viper.GetString("NAMESPACE"),
+		},
+		Spec: batchv1.JobSpec{
+			TTLSecondsAfterFinished: &ttlSecondsAfterFinished,
+			Template: v1.PodTemplateSpec{
+				Spec: v1.PodSpec{
+					Hostname: *jobName,
+					NodeSelector: map[string]string{
+						"operator-node": "true",
+					},
+					Containers: []v1.Container{
+						{
+							Resources: v1.ResourceRequirements{
+								Requests: map[v1.ResourceName]resource.Quantity{
+									"cpu":    resource.MustParse("4"),
+									"memory": resource.MustParse("2000"),
+									//TODO: Control resource requests from configuration
+								},
+							},
+							Name:    *jobName,
+							Image:   *image,
+							Command: strings.Split(*cmd, " "),
+							Env: []v1.EnvVar{
+								{
+									Name:  "DB_USER",
+									Value: viper.GetString("PGUSER"),
+								},
+								{
+									Name:  "DB_PWD",
+									Value: viper.GetString("PGPASSWORD"),
+								},
+								{
+									Name:  "DB_HOST",
+									Value: viper.GetString("PGHOST"),
+								},
+								{
+									Name:  "DB_NAME",
+									Value: viper.GetString("PGDATABASE"),
+								},
+								{
+									Name:  "ENV",
+									Value: viper.GetString("ENV"),
+								},
+								{
+									Name:  "DEBUG",
+									Value: viper.GetString("DEBUG"),
+								},
+							},
+						},
+					},
+					RestartPolicy: v1.RestartPolicyNever,
+					ImagePullSecrets: []v1.LocalObjectReference{
+						{
+							Name: "gitlabregistrypullsecret",
+						},
+					},
+				},
+			},
+			BackoffLimit: &backOffLimit,
+		},
+	}
+
+	log.Info().Msg(*jobName)
+	log.Info().Msg("Checking if Job is already exists.")
+	_, err := clientset.BatchV1().Jobs(viper.GetString("NAMESPACE")).Get(context.Background(), *jobName, metav1.GetOptions{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Job",
+			APIVersion: "batch/v1",
+		},
+	})
+	if err != nil {
+		log.Info().Msg("The Job does not exist. Creating one")
+		_, err := jobs.Create(context.Background(), jobSpec, metav1.CreateOptions{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Job",
+				APIVersion: "batch/v1",
+			},
+		})
+		if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			log.Error().Msg("Error creating job")
+			log.Error().Msg(statusError.ErrStatus.Message)
+			panic(err.Error())
+		}
+		log.Info().Msgf("Successfully created a new Job: %v", *jobName)
+		updateJobRecord(*jobName, id)
+
+	} else {
+		log.Info().Msgf("Job name %v already exists. Doing nothing", *jobName)
+	}
+
 }
